d47e46deceef2c341f5dbab580f1be0b9a187005
chadd williams <chadd980@hotmail.com>
2017-01-05 17:43:31 -0500

    first commit

---
+++ twitch.js
@@ -0,0 +1,14 @@
+$("document").ready(function() {
+  streamers = [
+    "freecodecamp",
+    "ESL_SC2",
+    "brunofin"
+  ]
+  for (var i = 0; i < streamers.length; i++) {
+    
+  }
+  var freeCodeCampUrl = "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp"
+  $.ajax ({
+
+  })
+})
