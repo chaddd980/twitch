68b23c0882dd311bbb5491a33047e43d1ee78f63
chadd williams <chadd980@hotmail.com>
2017-01-06 05:21:31 -0500

    added icons indicating channel status

--- twitch.js
+++ twitch.js
@@ -2,7 +2,14 @@ $("document").ready(function() {
   streamers = [
     "freecodecamp",
     "ESL_SC2",
-    "brunofin"
+    "brunofin",
+    "OgamingSC2",
+    "cretetion",
+    "storbeck",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas",
+    "comster404"
   ]
   var noStream = false;
   for (var i = 0; i < streamers.length; i++) {
@@ -21,7 +28,7 @@ $("document").ready(function() {
             var streamer = data["stream"]["channel"]["display_name"]
 
             $("ul").append("<div class='channels' id='" + streamer + "'><div class='logo-div col-xs-4 text-center'><a href='" + streamLink +
-            "'><img class='img-circle' src='" + streamLogo + "'></a><li>" + streamer + "</li></div><div class='game-info col-xs-8 text-center'>" + streamGame + "</div></div>")
+            "'><img class='img-circle' src='" + streamLogo + "'><li>" + streamer + "</li></a></div><div class='game-info col-xs-8 text-center'>" + streamGame + "  <img class='indicator' src='http://images.clipshrine.com/wheel/medium-yellow-glossy-circle-oval-shiny-0-15656.png'></div></div>")
           } else if (data["stream"] === null) {
             var streamer = streamers[i];
             $.ajax({
@@ -32,16 +39,16 @@ $("document").ready(function() {
               success: function(info) {
                 if (info["display_name"] === undefined) {
                   var streamGame = "Account Closed"
-                  var streamLogo = "https://inst.eecs.berkeley.edu/~cs194-26/fa14/upload/files/projFinalUndergrad/cs194-ma/sean_hickey_final_proj/pics/keyboard-random.jpg"
-                  $("ul").append("<div class='channels' id='" + streamer + "'><div class='logo-div col-xs-4 text-center'><a href='" + streamLink +
-                  "'><img class='img-circle' src='" + streamLogo + "'></a><li>" + streamer + "</li></div><div class='game-info col-xs-8 text-center'>" + streamGame + "</div></div>")
+                  var streamLogo = "http://etc.usf.edu/presentations/extras/letters/fridge_magnets/red/34/x-300.png"
+                  $("ul").append("<div class='channels' id='" + streamer + "'><div class='logo-div col-xs-4 text-center'><img class='img-circle' src='" + streamLogo +
+                  "'><li>" + streamer + "</li></div><div class='game-info col-xs-8 text-center'>" + streamGame + "  <img class='indicator' src='http://images.clipshrine.com/wheel/medium-yellow-glossy-circle-oval-shiny-66.6-15656.png'></div></div>")
                 } else {
                   streamer = info["display_name"];
                   var streamLogo = info["logo"];
                   var streamGame = "Offline";
                   var streamLink = info["url"];
                   $("ul").append("<div class='channels' id='" + streamer + "'><div class='logo-div col-xs-4 text-center'><a href='" + streamLink +
-                  "'><img class='img-circle' src='" + streamLogo + "'></a><li>" + streamer + "</li></div><div class='game-info col-xs-8 text-center'>" + streamGame + "</div></div>")
+                  "'><img class='img-circle' src='" + streamLogo + "'><li>" + streamer + "</li></a></div><div class='game-info col-xs-8 text-center'>" + streamGame + "  <img class='indicator' src='http://images.clipshrine.com/getimg/PngMedium-yellow-glossy-circle-oval-shiny-15656.png'></div></div>")
                 }
               }
             });
